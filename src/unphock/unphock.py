f57005fc5bbcb59fdc547687e081474db8e198ac
Nicolas Mokus <nmks@tuta.io>
2024-02-28 11:04:11 -0500

    FIX: option passed to warn

--- src/unphock/unphock.py
+++ src/unphock/unphock.py
@@ -31,11 +31,14 @@ def iterate(in_root: pathlib.Path, out_root: pathlib.Path, **kwargs):
                 phone_id = f"{int(phone_id):02d}"
             except ValueError:
                 warnings.warn(
-                    "Skipping {phone_id} that does not look like a phone ID", 1
+                    "Skipping {phone_id} that does not look like a phone ID",
+                    stacklevel=1,
                 )
                 continue
         else:
-            warnings.warn("Skipping {phone_id} that does not look like a phone ID", 1)
+            warnings.warn(
+                "Skipping {phone_id} that does not look like a phone ID", stacklevel=1
+            )
             continue
 
         xml_dir = path.joinpath("XML")
